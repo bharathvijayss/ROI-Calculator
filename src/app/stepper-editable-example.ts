cef75f1b01da3145d2a3dc44b53426a03ba34457
bharathvijayss <40032155+bharathvijayss@users.noreply.github.com>
2023-02-15 16:03:44 +0530

    Manager Resource Time Distribution Form Completed

--- src/app/stepper-editable-example.ts
+++ src/app/stepper-editable-example.ts
@@ -20,32 +20,68 @@ export class StepperEditableExample implements OnInit {
   costInputForm4 = this._formBuilder.group({
     B20: ['', Validators.required],
   });
-  costInputForm = this._formBuilder.group({
-    B16: new FormControl(),
-    B17: new FormControl(),
-    B18: new FormControl(5),
-    B19: new FormControl(),
-    B20: new FormControl(),
-    B21: new FormControl(5),
+  // costInputForm = this._formBuilder.group({
+  //   B16: new FormControl(),
+  //   B17: new FormControl(),
+  //   B18: new FormControl(5),
+  //   B19: new FormControl(),
+  //   B20: new FormControl(),
+  //   B21: new FormControl(5),
+  // });
+  resultConsolidatedForm: { [key: string]: number | string | null } = {
+    B18: 5,
+    B21: 5,
+    B28: 10,
+    B29: 5,
+  };
+
+  ManagerResourceTDForm1 = this._formBuilder.group({
+    B26: ['', Validators.required],
   });
+
+  ManagerResourceTDForm2 = this._formBuilder.group({
+    B27: ['', Validators.required],
+  });
+
+  ManagerResourceTDForm3 = this._formBuilder.group({
+    B30: [5, Validators.required],
+  });
+
+  radioOptions: string[] = ['Yes', 'No'];
+  min = 5;
+  max = 30;
+  step = 5;
 
   constructor(private _formBuilder: FormBuilder) {}
 
   ngOnInit() {
     const CostInputFormobservables = {
-      B16: this.costInputForm1.controls.B16.valueChanges.pipe(startWith(0)),
-      B17: this.costInputForm2.controls.B17.valueChanges.pipe(startWith(0)),
-      B19: this.costInputForm3.controls.B19.valueChanges.pipe(startWith(0)),
-      B20: this.costInputForm4.controls.B20.valueChanges.pipe(startWith(0)),
+      B16: this.costInputForm1.controls.B16.valueChanges.pipe(startWith(null)),
+      B17: this.costInputForm2.controls.B17.valueChanges.pipe(startWith(null)),
+      B19: this.costInputForm3.controls.B19.valueChanges.pipe(startWith(null)),
+      B20: this.costInputForm4.controls.B20.valueChanges.pipe(startWith(null)),
+      B26: this.ManagerResourceTDForm1.controls.B26.valueChanges.pipe(
+        startWith(null)
+      ),
+      B27: this.ManagerResourceTDForm2.controls.B27.valueChanges.pipe(
+        startWith(null)
+      ),
+      B30: this.ManagerResourceTDForm3.controls.B30.valueChanges.pipe(
+        startWith(5)
+      ),
     };
     combineLatest(CostInputFormobservables).subscribe({
       next: (latestVal) => {
-        this.costInputForm.patchValue({
+        this.resultConsolidatedForm = {
+          ...this.resultConsolidatedForm,
           B16: latestVal.B16,
           B17: latestVal.B17,
           B19: latestVal.B19,
           B20: latestVal.B20,
-        });
+          B26: latestVal.B26,
+          B27: latestVal.B27,
+          B30: latestVal.B30,
+        };
       },
     });
   }
