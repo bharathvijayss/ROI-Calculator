7cd34b0dd5aea61a0601a1f33c4355e53d2ef32e
bharathvijayss <40032155+bharathvijayss@users.noreply.github.com>
2023-02-15 14:02:32 +0530

    Cost Input Form Completed

--- src/app/stepper-editable-example.ts
+++ src/app/stepper-editable-example.ts
@@ -1,27 +1,52 @@
-import {Component} from '@angular/core';
-import {FormBuilder, Validators} from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
+import { combineLatest, startWith } from 'rxjs';
 
-/**
- * @title Stepper with editable steps
- */
 @Component({
   selector: 'stepper-editable-example',
   templateUrl: 'stepper-editable-example.html',
   styleUrls: ['stepper-editable-example.css'],
 })
-export class StepperEditableExample {
-  firstFormGroup = this._formBuilder.group({
-    firstCtrl: ['', Validators.required],
+export class StepperEditableExample implements OnInit {
+  costInputForm1 = this._formBuilder.group({
+    B16: ['', Validators.required],
   });
-  secondFormGroup = this._formBuilder.group({
-    secondCtrl: ['', Validators.required],
+  costInputForm2 = this._formBuilder.group({
+    B17: ['', Validators.required],
   });
-  isEditable = false;
+  costInputForm3 = this._formBuilder.group({
+    B19: ['', Validators.required],
+  });
+  costInputForm4 = this._formBuilder.group({
+    B20: ['', Validators.required],
+  });
+  costInputForm = this._formBuilder.group({
+    B16: new FormControl(),
+    B17: new FormControl(),
+    B18: new FormControl(5),
+    B19: new FormControl(),
+    B20: new FormControl(),
+    B21: new FormControl(5),
+  });
 
   constructor(private _formBuilder: FormBuilder) {}
+
+  ngOnInit() {
+    const CostInputFormobservables = {
+      B16: this.costInputForm1.controls.B16.valueChanges.pipe(startWith(0)),
+      B17: this.costInputForm2.controls.B17.valueChanges.pipe(startWith(0)),
+      B19: this.costInputForm3.controls.B19.valueChanges.pipe(startWith(0)),
+      B20: this.costInputForm4.controls.B20.valueChanges.pipe(startWith(0)),
+    };
+    combineLatest(CostInputFormobservables).subscribe({
+      next: (latestVal) => {
+        this.costInputForm.patchValue({
+          B16: latestVal.B16,
+          B17: latestVal.B17,
+          B19: latestVal.B19,
+          B20: latestVal.B20,
+        });
+      },
+    });
+  }
 }
-
-
-/**  Copyright 2023 Google LLC. All Rights Reserved.
-    Use of this source code is governed by an MIT-style license that
-    can be found in the LICENSE file at https://angular.io/license */
