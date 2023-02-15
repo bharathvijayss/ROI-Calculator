83802b54e69500d8d6fb352feda6c4ea6db109e1
bharathvijayss <40032155+bharathvijayss@users.noreply.github.com>
2023-02-15 13:03:51 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ROI-Calculator
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ht2c34-qjm7ny)
---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+import {MaterialExampleModule} from '../material.module';
+import {StepperEditableExample} from './stepper-editable-example';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatNativeDateModule} from '@angular/material/core';
+import {HttpClientModule} from '@angular/common/http';
+
+@NgModule({
+  declarations: [StepperEditableExample],
+  imports: [
+    BrowserAnimationsModule,
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    MatNativeDateModule,
+    MaterialExampleModule,
+    ReactiveFormsModule,
+  ],
+  providers: [],
+  bootstrap: [StepperEditableExample],
+})
+export class AppModule {}
